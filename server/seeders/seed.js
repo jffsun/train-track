4efcc20bf5edb3d2064ec7547b89b22d7ec36201
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2023-03-31 19:09:12 -0700

    Sample seed for Exercises and Sets

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,22 +1,29 @@
 const db = require('../db/conn');
-const { User } = require('../models');
-const { Workout } = require('../models');
-
 const userSeeds = require('./sampleUserSeed.json')
 const workoutSeeds = require('./sampleWorkoutSeed.json')
-// const { User, Workout, Exercise, Set } = require('../models');
+const exerciseSeeds = require('./sampleExerciseSeed.json')
+const setSeeds = require('./sampleSetSeed.json')
+
+const { User, Workout, Exercise, Set } = require('../models');
 
 db.once('open', async () => {
   try {
 
     // delete existing data 
     // await User.deleteMany({});
-    await Workout.deleteMany({});
+    // await Workout.deleteMany({});
     // await Exercise.deleteMany({});
     // await Set.deleteMany({});
 
     // await User.insertMany(userSeeds);
-    await Workout.insertMany(workoutSeeds);
+    // await Workout.insertMany(workoutSeeds);
+    // await Exercise.insertMany(exerciseSeeds);
+    await Set.insertMany(setSeeds);
+
+    // Problem: Deleting and reseeding creates new objectIds for seeded data so we cannot seed data all at once
+    // Problematic because we need the objectIds beforehand before seeding data that references a parent object
+    // Solution 1: Do not reseed data 
+    // Solution 2: Figure out way to generate objectId, take new objectId, create new seed with new parent's objectId.
   } catch (err) {
     console.error(err);
 
